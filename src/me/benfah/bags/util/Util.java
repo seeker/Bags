620bacd5b6dff7b23e86482d0247ed59c70c7c4a
benfah <vayegame@gmail.com>
2016-04-12 13:40:17 +0200

    Added method
    Added sendRequest method

--- src/me/benfah/bags/util/Util.java
+++ src/me/benfah/bags/util/Util.java
@@ -1,9 +1,14 @@
 package me.benfah.bags.util;
 
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import me.benfah.bags.main.Bags;
 
 public class Util {
 
+	public static String standardLink = "https://www.dropbox.com/s/kllx5y7aqv5x317/Bags2.zip?dl=1";
+	
 	public static boolean isUnbreakableAndHasDamage(ItemStack stack, short damage)
 	{
 		if(stack.getItemMeta().spigot().isUnbreakable())
@@ -11,5 +16,14 @@ public class Util {
 		return true;
 		return false;
 	}
-
+	
+	public static void sendRequest(Player p)
+	{
+		if(Bags.cfg.getBoolean("custom-resourcepack"))
+		{
+			p.setResourcePack(Bags.cfg.getString("custom-resourcepack-link"));
+		}
+		else
+		p.setResourcePack(standardLink);
+	}
 }
