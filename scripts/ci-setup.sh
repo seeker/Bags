81c767b5359d418cdfd743eba529d7f308b4fc21
Nicholas Wright <dozedoffagain@gmail.com>
2016-06-12 04:25:44 +0200

    Initial Travis CI config

---
+++ scripts/ci-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Download the latest buildtool
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+
+# TODO use maven or environment variable to set revision
+# generate the dependencies
+java -jar BuildTools --rev 1.9
